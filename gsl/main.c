b9b76932682f5bcfd6b81ff7ce81c72c2e42ddfa
kuhlwein <Morten@kuhlwein.dk>
2018-02-20 17:04:27 +0100

    gsl exercise 1

---
+++ gsl/main.c
@@ -0,0 +1,13 @@
+#include <gsl/gsl_sf_airy.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+	for (double i = -2; i < 2; i=i+0.01) {
+		double Ai = gsl_sf_airy_Ai(i, GSL_PREC_DOUBLE);
+		double Bi = gsl_sf_airy_Bi(i, GSL_PREC_DOUBLE);
+		printf("%lg \t %lg \t %lg\n",i,Ai,Bi);
+	}
+	return 0;
+}
