cd77eef12285bf9f8945cb7004d6c93ad6bbc2f3
Morten Kuhlwein <Morten@Kuhlwein.dk>
2018-02-27 12:49:59 +0100

    integ exercise 1

---
+++ integ/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_errno.h>
+
+double integrand(double x, void* params) {
+	return log(x)/sqrt(x);
+}
+
+int main(int argc, char *argv[])
+{
+	//exercise 1
+	gsl_function f;
+	f.function = integrand;
+	int limit = 100;
+	gsl_integration_workspace* workspace = gsl_integration_workspace_alloc(limit);
+	double result, err;
+	int status = gsl_integration_qags(&f,0,1,1e-6,1e-6,limit,workspace,&result,&err);
+	if (status!=GSL_SUCCESS) err=NAN;
+	printf("int_0^1 ln(x)/sqrt(x) dx=%f\n", result);
+
+
+	
+
+	return 0;
+}
