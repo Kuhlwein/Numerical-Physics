5f7d76eace62f395810d0f363bf46e121023515f
kuhlwein <Morten@kuhlwein.dk>
2018-03-20 17:56:05 +0100

    spline plots

--- numeric/interpolation/main.cc
+++ numeric/interpolation/main.cc
@@ -6,17 +6,21 @@
 using namespace std;
 
 int main() {
-	int n = 20;
+	int n = 10;
 	vector<double> v(n), vv(n);
 
-	for(int i=0; i<n; i++) {v[i]=i; vv[i]=sin(i);}
+	for(int i=0; i<n; i++) {v[i]=i; vv[i]=sin(i)*i; cout<<i<<"\t"<<sin(i)*i<<"\n";}
+	cout << "\n\n";
 
-	function<double(double)> fun = cspline(v,vv);
-	function<double(double)> fun2 = qspline(v,vv);
-	function<double(double)> fun3 = lspline(v,vv);
+	function<double(double)> funl = lspline(v,vv), funli = lsplineInteg(v,vv), funq = qspline(v,vv),
+		funqd = qsplineDiff(v,vv), funqi = qsplineInteg(v,vv), func = cspline(v,vv),
+		funcd = csplineDiff(v,vv), funci = csplineInteg(v,vv);
 
-	for(double i=0.; i<18; i=i+0.05) {
-		cout << i << "\t" << fun(i) << "\t" << fun2(i) << "\t" << fun3(i) << "\n";
+	for(double i=0.; i<9; i=i+0.05) {
+		cout << i << "\t" << funl(i) << "\t" << funli(i) << "\t" 
+			<< funq(i) << "\t" << funqd(i) << "\t" << funqi(i) << "\t" 
+			<< func(i) << "\t" << funcd(i) << "\t" << funci(i) << "\t"
+			<< sin(i)-i*cos(i) << "\t" << sin(i)+i*cos(i) << "\n";
 	}
 
 	return 0;
