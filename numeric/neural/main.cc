c48018e23f63b5ec8accb5d398beef9165458008
kuhlwein <Morten@kuhlwein.dk>
2018-05-15 16:37:11 +0200

    Better plot, and deeper network for number recognition

--- numeric/neural/main.cc
+++ numeric/neural/main.cc
@@ -30,7 +30,7 @@ function<vec(vec)> dsoftplus = [](vec x) {
 
 int main() {
 	cout << "Number recognition at the bottom!\n";
-	Network *n = new Network({1,2,1},{wav, f},{dwav, df});
+	Network *n = new Network({1,5,1},{wav, f},{dwav, df});
 	vector<vec> in(50), out(50);
 	for (int i=0; i<50; i++) {
 		vec v = vec(1,fill::randn);
@@ -42,22 +42,23 @@ int main() {
 		cout << i << "\t" << e.at(0) << "\t" << sin(5*i)*exp(-10*i*i)<< "\n";
 	}
 	
-	Network *n2 = new Network({2,6,1},{wav, f},{dwav, df});
-	vector<vec> in2(50), out2(50);
+	Network *n2 = new Network({2,15,15,1},{wav,f, f},{dwav,df, df});
 	for (int i=0; i<50; i++) {
 		vec v = vec(2,fill::randn);
-		in[i] = v; out[i]={sin(5*v[0])+cos(1*v[1])};
+		in[i] = v; out[i]={sin(5*v[0])+cos(5*v[1])};
 	}
-	n2->train(in,out,2,0.01,4);
+	n2->train(in,out,0.5,0.1,4);
+	n2->train(in,out,0.2,0.04,4);
+	n2->train(in,out,0.05,0.005,4);
 	cout << "\n\n";
 	for (double i=0; i<1; i+=0.02) for(double j=0; j<1; j+=0.02) {
 		vec e = n2->eval({i, j});
-		cout << i << "\t" << j << "\t" << e.at(0) << "\t" << sin(5*i)+cos(1*j)<< "\n";
+		cout << i << "\t" << j << "\t" << e.at(0) << "\t" << sin(5*i)+cos(5*j)<< "\n";
 	}
 	cout << "\n\n";
 
 	#include "letters.h"
-	Network *n3 = new Network({35,5,5,10},{softplus, softplus, softmax},{dsoftplus, dsoftplus, dsoftmax});
+	Network *n3 = new Network({35,7,7,7,10},{softplus, softplus,softplus, softmax},{dsoftplus,dsoftplus, dsoftplus, dsoftmax});
 	
 	function<vec(vec,int,int)> shiftnum = [](vec x,int vert, int hors) {
 		mat number = reshape(x,5,7);
@@ -71,11 +72,10 @@ int main() {
 	vector<vec> input, output, num = {l1,l2,l3,l4,l5,l6,l7,l8,l9,l0};
 	for (int i=0; i<100; i++) for(int count=0; count<10; count++) {
 		vec randvec = vec(2,fill::randu)*4-2;
-		//vec candidate = shiftnum(num[count],(int)randvec[0],(int)randvec[1]);
 		input.push_back(num[count]);
 		output.push_back(answer.col(count));
 	}
-	n3->train(input,output,2,0.05,5);
+	n3->train(input,output,0.1,0.01,5);
 
 	function<vec(vec)> distortnum = [](vec x) {
 		vec a = vec(1,fill::randu)*35;
